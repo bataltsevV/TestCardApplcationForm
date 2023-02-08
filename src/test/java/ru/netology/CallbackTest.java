fe6cc15b15397110ac1896ee527586a7e8d8a5ea
bataltsevV <mr.podstrelennyy@mail.ru>
2023-02-08 18:53:10 +0300

    Added jar file, changed test

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -7,11 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,7 +17,7 @@ public class CallbackTest {
 
     @BeforeAll
     static void setUpAll() {
-        WebDriverManager.chromedriver().create();
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
@@ -29,7 +26,7 @@ public class CallbackTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
     }
 
     @AfterEach
@@ -39,15 +36,14 @@ public class CallbackTest {
     }
 
     @Test
-    void SendFormTest() throws InterruptedException {
+    void sendFormTest() {
         driver.get("http://localhost:9999");
-        List<WebElement> inputs = driver.findElements(By.tagName("input"));
-        inputs.get(0).sendKeys("Дмитриев Дмитрий");
-        inputs.get(1).sendKeys("+79027993131");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Дед-Лайн Дмитрий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79027993131");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__text")).click();
         String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals(expected, actual);
     }
 }
